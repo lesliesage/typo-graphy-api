5565a36484254cfad68833483568c109860f68ef
lesliesage <leslie.sage@gmail.com>
2019-09-26 12:18:38 -0400

    added placeholders for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   resources :bookmarks, only: [:index, :new, :create, :show, :destroy]
   resources :help_articles, only: [:index, :show]
 
-  get '/users/:username', to: 'users#show'
-  get '/token', to: 'users#token'
   get '/queue', to: 'snippets#queue'
   get '/medians', to: 'tests#medians'
-  
+  get '/signup', to: 'users#create'
+  get '/users/:username', to: 'users#show'
+  get '/token', to: 'users#token'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
