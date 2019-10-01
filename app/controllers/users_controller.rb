3b2d1cd0bd6011530b37e3fc3ed2ab50ff6ebbfc
lesliesage <leslie.sage@gmail.com>
2019-09-30 23:06:17 -0400

    can create user; progress toward profile pg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,32 @@
 require 'jwt'
+require 'byebug'
 class UsersController < ApplicationController
-    # skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
-    def show
-      token = request.headers["Authentication"].split(" ")[1]
-      user = User.find(decode(token)["user_id"])
-      render json: {
-          authenticated: true,
-          message: "logging in...",
-          user: user,
-          # user: user.to_json(user_serializer),
-          token: token
-      }, status: :accepted
-    end
+  def show
+    token = request.headers["Authentication"]
+    render json: User.find(decode(token)["user_id"]), status: :accepted
+  end
 
-    def create
-        user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
-      end
+  def create
+    user = User.create({username: params[:username], email: params[:email], password: params[:password]})
+    token = encode(user_id: user.id)
+    render json: {
+      authenticated: true,
+      message: "logging in...",
+      user: user.to_json(user_serializer),
+      token: token
+    }, status: :accepted
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:username, :email, :password) 
-    end
+  def user_params
+      params.require(:user).permit(:username, :email, :password) 
+  end
 
-    def user_serializer
-      {
-        :only => [:id, :username, :email]
-      }
-    end
+  def user_serializer
+    {:except => [:created_at, :updated_at]}
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :snippets, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :users, only: [:new, :create, :edit, :update, :destroy]
+  resources :users, only: [:new, :edit, :update, :destroy]
   resources :tests, only: [:index, :create]
   # resources :languages, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # resources :bookmarks, only: [:index, :new, :create, :show, :destroy]
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   get '/queue', to: 'snippets#queue'
   get '/medians', to: 'tests#medians'
-  get '/signup', to: 'users#create'
+  post '/signup', to: 'users#create'
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#show'
 
