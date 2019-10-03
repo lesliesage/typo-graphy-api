730c535eca614337c93a7ceb1002f2e0101e2b80
lesliesage <leslie.sage@gmail.com>
2019-10-03 17:17:53 -0400

    skipped auth for help page

--- app/controllers/help_articles_controller.rb
+++ app/controllers/help_articles_controller.rb
@@ -1,4 +1,7 @@
 class HelpArticlesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    skip_before_action :authorized
+    
     def index
         help_articles = HelpArticle.all
         render json: help_articles.to_json(help_serializer)
