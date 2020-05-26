b6944ce8ceca1f9fabbd041c74694a08f468096b
lesliesage <leslie.sage@gmail.com>
2020-05-26 12:36:46 -0400

    don't compile assets in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # Don't compile assets in production. Assets are pre-compiled automatically by Heroku on deploy.
+  config.assets.compile = false
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
